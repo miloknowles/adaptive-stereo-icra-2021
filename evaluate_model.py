3646f61ae7b335434170a8c7e5a0f1635d29b7dd
Milo Knowles <mknowles@mit.edu>
2021-05-18 16:40:22 -0400

    Vidieo saving ability

--- evaluate_model.py
+++ evaluate_model.py
@@ -6,6 +6,7 @@
 
 import argparse
 import os, sys
+from utils.path_utils import path_to_output
 
 import torch
 from torch.utils.data import DataLoader
@@ -38,7 +39,7 @@ def main(opt):
 
   if opt.mode == "save":
     feature_net = FeatureExtractorNetwork(opt.stereonet_k).cuda()
-    stereo_net = StereoNet(k=opt.stereonet_k, r=1).cuda()
+    stereo_net = StereoNet(opt.stereonet_k, 1, 0).cuda()
     feature_net.load_state_dict(torch.load(os.path.join(opt.load_weights_folder, "feature_net.pth")), strict=True)
     stereo_net.load_state_dict(torch.load(os.path.join(opt.load_weights_folder, "stereo_net.pth")), strict=True)
 
@@ -55,11 +56,14 @@ def main(opt):
 
         lines = dataset.lines[opt.batch_size*i:opt.batch_size*(i+1)]
         left_feat, right_feat = feature_net(inputs["color_l/0"]), feature_net(inputs["color_r/0"])
+        # outputs = stereo_net(inputs["color_l/0"], left_feat, right_feat, "l",
+        #                     store_feature_gradients=False,
+        #                     store_refinement_gradients=False,
+        #                     do_refinement=True,
+        #                     output_cost_volume=True)
+
         outputs = stereo_net(inputs["color_l/0"], left_feat, right_feat, "l",
-                            store_feature_gradients=False,
-                            store_refinement_gradients=False,
-                            do_refinement=True,
-                            output_cost_volume=True)
+                    output_cost_volume=True)
 
         # Save all network outputs.
         for key in outputs:
@@ -95,6 +99,29 @@ def main(opt):
       cv.imshow("gt_disp_l/0", gt_disp_l_0)
       cv.waitKey(0)
 
+  elif opt.mode == "video":
+    output_folder = path_to_output("video")
+
+    for i, inputs in enumerate(dataset):
+      if opt.frames > 0 and i >= opt.frames:
+        break
+
+      color_l_0 = tensor_to_cv_rgb(inputs["color_l/0"].contiguous())
+      gt_disp_l_0 = visualize_disp_cv(inputs["gt_disp_l/0"].contiguous(), cmap=plt.get_cmap("inferno"), vmin=0, vmax=0.6*192)
+
+      pred_disp_l_0 = torch.load(get_save_filename(save_folder, "pred_disp_l/0", i)).contiguous()
+
+      EPE = torch.abs(pred_disp_l_0.cpu() - inputs["gt_disp_l/0"].cpu())[inputs["gt_disp_l/0"].cpu() > 0].mean()
+      print("EPE:", EPE)
+      pred_disp_l_0 = visualize_disp_cv(pred_disp_l_0, cmap=plt.get_cmap("inferno"), vmin=0, vmax=0.6*192)
+
+      cv.putText(color_l_0, "Frame {}".format(i), (50, 50), cv.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2, cv.LINE_AA)
+      cv.putText(pred_disp_l_0, "EPE: {:.02f}".format(EPE), (50, 50), cv.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2, cv.LINE_AA)
+
+      cv.imwrite(path_to_output("video/left_{:05d}.png".format(i)), color_l_0)
+      cv.imwrite(path_to_output("video/gt_{:05d}.png".format(i)), gt_disp_l_0)
+      cv.imwrite(path_to_output("video/pred_{:05d}.png".format(i)), pred_disp_l_0)
+
   elif opt.mode == "eval":
     pass
 
@@ -105,7 +132,7 @@ def main(opt):
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Options for evaluating depth prediction")
   parser.add_argument("--mode", type=str,
-                      choices=["save", "playback", "eval"])
+                      choices=["save", "playback", "eval", "video"])
 
   parser.add_argument("--dataset_path", type=str, help="Top level folder for the dataset being used")
   parser.add_argument("--dataset_name", type=str, help="Which dataset to evaluate on")
@@ -123,6 +150,7 @@ if __name__ == "__main__":
   parser.add_argument("--max_disp_viz", type=float, default=300, help="Divide the disparity by this to normalize it for viewing")
   parser.add_argument("--window_sf", type=float, default=1.5, help="Scale factor for OpenCV windows")
   parser.add_argument("--wait", default=False, action="store_true", help="If set, user must keypress through all images")
+  parser.add_argument("--frames", default=-1, type=int, help="Number of playback frames")
 
   # parser.add_argument("--do_vertical_flip", default=False, action="store_true", help="Vertically flip input images")
   # parser.add_argument("--predict_variance", default=False, action="store_true", help="Save variance predictions from the network (MGC-Net only)")
---
+++ evaluation/scripts/visualize_domain_shift.sh
@@ -0,0 +1,61 @@
+#============================ SAVE DATA ===============================
+# echo "Saving data for TRAINING DOMAIN"
+# python ../../evaluate_model.py \
+#   --mode save \
+#   --dataset_path ~/datasets/virtual_kitti/ \
+#   --dataset_name VirtualKitti \
+#   --split virtual_kitti_clone \
+#   --load_weights_folder ../../resources/pretrained_models/stereo_net/vk_clone_368x960_16X \
+#   --height 368 \
+#   --width 960 \
+#   --stereonet_k 4 \
+#   --subsplit train \
+#   --scales 0 \
+#   --batch_size 8 \
+
+# echo "Saving data for NOVEL DOMAIN"
+# python ../../evaluate_model.py \
+#   --mode save \
+#   --dataset_path ~/datasets/virtual_kitti/ \
+#   --dataset_name VirtualKitti \
+#   --split virtual_kitti_rain \
+#   --load_weights_folder ../../resources/pretrained_models/stereo_net/vk_clone_368x960_16X \
+#   --height 368 \
+#   --width 960 \
+#   --stereonet_k 4 \
+#   --subsplit train \
+#   --scales 0 \
+#   --batch_size 8 \
+
+python ../../evaluate_model.py \
+  --mode video \
+  --frames 100 \
+  --dataset_path ~/datasets/virtual_kitti/ \
+  --dataset_name VirtualKitti \
+  --split virtual_kitti_clone \
+  --load_weights_folder ../../resources/pretrained_models/stereo_net/vk_clone_368x960_16X \
+  --height 368 \
+  --width 960 \
+  --stereonet_k 4 \
+  --subsplit train \
+  --scales 0 \
+  --batch_size 1 \
+
+# python ../../evaluate_model.py \
+#   --mode video \
+#   --frames 100 \
+#   --dataset_path ~/datasets/virtual_kitti/ \
+#   --dataset_name VirtualKitti \
+#   --split virtual_kitti_rain \
+#   --load_weights_folder ../../resources/pretrained_models/stereo_net/vk_clone_368x960_16X \
+#   --height 368 \
+#   --width 960 \
+#   --stereonet_k 4 \
+#   --subsplit train \
+#   --scales 0 \
+#   --batch_size 1 \
+
+# Run these commands to generate video:
+# convert -delay 5 -loop 0 pred*.png 0pred.gif
+# convert -delay 5 -loop 0 left*.png 0left.gif
+# https://askubuntu.com/questions/648244/how-do-i-create-an-animated-gif-from-still-images-preferably-with-the-command-l
